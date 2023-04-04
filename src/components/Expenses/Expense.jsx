10bb1adc63db60d16707e8928549c18fca6f4ace
Himanshudevg2000 <himanshudevgade87@gmail.com>
2023-04-04 15:38:43 +0530

    add and get expenses

--- src/components/Expenses/Expense.jsx
+++ src/components/Expenses/Expense.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import classes from './Expense.module.css'
 
 const Expense = () => {
@@ -6,21 +6,73 @@ const Expense = () => {
     const [amount, setAmount] = useState('');
     const [description, setDescription] = useState('');
     const [category, setCategory] = useState('');
-    const [list, setList] = useState('')
+    const [list, setList] = useState(false)
+    const [addexpense, setAddExpense] = useState([])
 
-    const submitHandler = () => {
-        setList(amount, description, category)
-        console.log(amount, description, category);
+    let userDetail = JSON.parse(localStorage.getItem('user'))
+    let id = userDetail.id
+    console.log(id)
+
+
+    let collectData = async (e) => {
+        try {
+            e.preventDefault();
+            let result = await fetch("http://localhost:4000/api/v1/expense/addexpense", {
+                method: "Post",
+                body: JSON.stringify({ userId: id, amount, description, category }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            if (!result.ok) {
+                throw new Error('Something went wrong')
+            }
+            result = await result.json();
+            console.log(result);
+            setList(true)
+            alert("Expense added")
+        }
+        catch (error) {
+            alert("something went wrong")
+        }
     }
 
-    // const expenseList = list.map((items) => {
-    //     return (<ul>
-    //         <li>{items.amount}</li>
-    //         <li>{items.description}</li>
-    //         <li>{items.category}</li>
-    //     </ul>)
-    // })
+    let data;
+    let apiHandler = async () => {
+        try {
+            let result = await fetch('http://localhost:4000/api/v1/expense/getexpense/642567fe5bdb7d1c3ccbfbcd')
+            data = await result.json();
+            // console.log(data)
+            const datalist = data.data.map((items) => {
+                return {
+                    amount: items.amount,
+                    description: items.description,
+                    category: items.category,
+                }
+            })
+            setAddExpense(datalist)
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
 
+    useEffect(() => {
+        apiHandler()
+    }, []);
+    console.log(addexpense)
+
+    const expenseList = addexpense.map((items) => {
+        return (
+            <div>
+                <div className={classes.expensediv}>
+                    <ul className={classes.expenseul} >{items.amount}</ul>
+                    <ul className={classes.expenseul}>{items.description}</ul>
+                    <ul className={classes.expenseul}>{items.category}</ul>
+                </div>
+            </div>
+        )
+    })
 
     return (
         <Fragment>
@@ -59,12 +111,15 @@ const Expense = () => {
                     <option value="travel">travel</option>
                     <option value="other">other</option>
                 </select>
-                <button className={classes.btn} onClick={submitHandler} >Add</button>
+                <button className={classes.btn} onClick={collectData} >Add</button>
             </div>
 
             <div className={classes.box}>
                 <h3>Expense List</h3>
-                {/* {expenseList} */}
+                <h4>Amount</h4>
+                <h4>Description</h4>
+                <h4>Category</h4>
+                {expenseList}
             </div>
         </Fragment>
     )
